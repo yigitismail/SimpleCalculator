a765708c85fdf64806282bece6a7da42aa3f5c5d
“yigitismail” <smlygt@icloud.com>
2020-01-20 19:35:05 +0300

    ilk hali

---
+++ app/src/main/java/com/yigitismail/simplecalculator/MainActivity.java
@@ -0,0 +1,33 @@
+package com.yigitismail.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sum(View view){
+
+    }
+
+    public void deduct(View view){
+
+    }
+
+    public void multiply(View view){
+
+    }
+
+    public void divide(View view){
+
+    }
+
+
+}
