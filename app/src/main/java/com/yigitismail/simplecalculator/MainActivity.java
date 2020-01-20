664ce4b9cb2a2518bf431a69be0159c027a1e168
“yigitismail” <smlygt@icloud.com>
2020-01-20 19:52:51 +0300

    ikinci hali

--- app/src/main/java/com/yigitismail/simplecalculator/MainActivity.java
+++ app/src/main/java/com/yigitismail/simplecalculator/MainActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1Text;
+    EditText number2Text;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1Text = findViewById(R.id.number1Text);
+        number2Text = findViewById(R.id.number2Text);
+        resultText = findViewById(R.id.resultText);
+
+
     }
 
     public void sum(View view){
+
+        
 
     }
 
